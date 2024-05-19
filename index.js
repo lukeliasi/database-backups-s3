6fd75ab0de65e634901ce567aa7d96888d28156e
Gyom03 <guillaumerichard03@gmail.com>
2024-05-19 21:32:37 +0200

    fixe for aws sdk v3

--- index.js
+++ index.js
@@ -22,8 +22,10 @@ function loadConfig() {
 
   return {
     aws: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      credentials: {
+        accessKeyId:  process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey:  process.env.AWS_SECRET_ACCESS_KEY
+      },
       region: process.env.AWS_S3_REGION,
       endpoint: process.env.AWS_S3_ENDPOINT,
       s3_bucket: process.env.AWS_S3_BUCKET
