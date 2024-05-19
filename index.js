49892b772a095818f846eebae50685b279ce1599
Gyom03 <guillaumerichard03@gmail.com>
2024-05-19 22:16:40 +0200

    fixed db error

--- index.js
+++ index.js
@@ -24,10 +24,6 @@ function loadConfig() {
     aws: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      credentials: {
-        accessKeyId:  process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey:  process.env.AWS_SECRET_ACCESS_KEY 
-      },
       region: process.env.AWS_S3_REGION,
       endpoint: process.env.AWS_S3_ENDPOINT,
       s3_bucket: process.env.AWS_S3_BUCKET
