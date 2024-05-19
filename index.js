81a100e3d817398817c55ed8893ba9b9ce51b617
Gyom03 <guillaumerichard03@gmail.com>
2024-05-19 21:44:34 +0200

    what

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ async function processBackup() {
         return;
     }
 
-    try {
+   
       // 1. Execute the dump command
       await exec(dumpCommand);
 
@@ -110,9 +110,7 @@ async function processBackup() {
       await s3Client.send(putCommand);
       
       console.log(`✓ Successfully uploaded db backup for database ${dbType} ${dbName} ${dbHostname}.`);
-    } catch (error) {
-      console.error(`An error occurred while processing the database ${dbType} ${dbName}, host: ${dbHostname}): ${error}`);
-    }
+    
   }
 }
 
